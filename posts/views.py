84370027a17f08c9a05523f233732edf91d7d1d4
Suraj Kumar <codeartisanlab2607@gmail.com>
2021-06-24 23:07:40 +0530

    Django Rest API with user authentication

---
+++ posts/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from rest_framework import generics,permissions
+
+from .models import Post
+from .serializer import PostSerializer
+
+class PostList(generics.ListCreateAPIView):
+	permission_classes=(permissions.IsAuthenticated,)
+	queryset=Post.objects.all()
+	serializer_class=PostSerializer
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+	permission_classes=(permissions.IsAuthenticated,)
+	queryset=Post.objects.all()
+	serializer_class=PostSerializer
